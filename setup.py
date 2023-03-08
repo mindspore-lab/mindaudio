4def6555daba3e7abaea4a1c81330d39a395454f
dyz <32426751+DongyaoZhu@users.noreply.github.com>
2023-03-08 18:58:06 +0800

    setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 from setuptools.command.build_py import build_py
 from setuptools.command.egg_info import egg_info
 
-from mindaudio.version import __version__
+exec(open('mindaudio/version.py').read())
 
 version = __version__
 package_name = 'mindaudio'
