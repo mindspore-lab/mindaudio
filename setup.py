2a75418918960c0c90039421fd7fc3116412a40a
vigo999 <zwiori1982@163.com>
2023-06-26 00:41:44 +0800

    update setup file with more infomation

--- setup.py
+++ setup.py
@@ -2,51 +2,46 @@
 
 from setuptools import find_packages, setup
 
+with open("README.md", mode="r", encoding="utf-8") as f:
+    long_description = f.read()
+
+with open("requirements.txt", mode="r", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
 exec(open("mindaudio/version.py").read())
-
-
-def read_requirements(fps):
-    reqs = []
-    for fp in fps:
-        with open(fp) as f:
-            reqs.extend(f.readlines())
-
-    return reqs
-
 
 setup(
     name="mindaudio",
     author="MindSpore Lab",
     author_email="mindspore-lab@example.com",
+    version=__version__,
     url="https://github.com/mindspore-lab/mindaudio",
     project_urls={
         "Sources": "https://github.com/mindspore-lab/mindaudio",
         "Issue Tracker": "https://github.com/mindspore-lab/mindaudio/issues",
     },
     description="A toolbox of audio models and algorithms based on MindSpore.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="Apache Software License 2.0",
     include_package_data=True,
     packages=find_packages(include=["mindaudio", "mindaudio.*"]),
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=[
-        "numpy >= 1.17.0",
-        "pyYAML >= 5.3",
+        "numpy",
+        "pyyaml",
         "tqdm",
+        "scipy",
+        "sentencepiece",
     ],
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
+        "Development Status :: 3 - Alpha"
         "License :: OSI Approved :: Apache Software License",
-        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
-    test_suite="tests",
-    tests_require=[
-        "pytest",
-    ],
-    version=__version__,
     zip_safe=False,
 )
